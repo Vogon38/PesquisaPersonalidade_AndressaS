60f9316aaad97f33dd7eb56ed9a278bad41d92b2
Vogon38 <luana@thecyborg.xyz>
2023-02-23 08:41:41 -0300

    finish

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Survey } from 'survey-react-ui';
+import { Chart } from 'chart.js/auto';
+import 'survey-core/defaultV2.min.css';
+import './App.css';
+import { Model } from 'survey-core';
+import { surveyJson } from './json.js';
+
+const chartColors = ["#4dc9f6", "#f67019", "#f53794", "#537bc4", "#acc236", "#166a8f", "#00a950"];
+
+function App() {
+  const survey = useRef(new Model(surveyJson)).current;
+  const [surveyResults, setSurveyResults] = useState(null);
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    // Load Chart.js library
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      // Cleanup Chart.js library
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  survey.onComplete.add((sender, options) => {
+    console.log(JSON.stringify(sender.data, null, 3));
+    setSurveyResults(survey.data);
+  });
+
+  useEffect(() => {
+    if (surveyResults) {
+      const totalChoices = Object.values(surveyResults).flat().length;
+      const chartData = Object.entries(surveyResults).map(([name, choices], index) => {
+        const count = choices.length;
+        return {
+          name: name,
+          label: `${name} (${count})`,
+          data: [count, totalChoices],
+          backgroundColor: chartColors[index % chartColors.length],
+          borderColor: chartColors[index % chartColors.length],
+          borderWidth: 1,
+        };
+      });
+      setChartData({
+        labels: [''],
+        datasets: chartData,
+      });
+    }
+  }, [surveyResults]);
+
+  useEffect(() => {
+    if (chartData && chartData.datasets) {
+      const ctx = document.getElementById('survey-results-chart').getContext('2d');
+      new Chart(ctx, {
+        type: 'bar',
+        data: chartData,
+        options: {
+          maintainAspectRatio: false, // disable aspect ratio so chart can be responsive
+          scales: {
+            y: {
+              beginAtZero: true,
+              stepSize: 1,
+              precision: 0
+            }
+          },
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  var label = chartData.labels[context.dataIndex];
+                  var value = context.dataset.data[context.dataIndex];
+                  var count = value + ' response' + (value !== 1 ? 's' : '');
+                  if (context.dataset.hasOwnProperty('datasets')) {
+                    return label + ': ' + count;
+                  } else {
+                    return count;
+                  }
+                }
+              }
+            }
+          }
+        }
+      });
+    }
+  }, [chartData]);
+
+  const onComplete = (survey, options) => {
+    setSurveyResults(survey.data);
+  };
+
+  return (
+    <>
+      <Survey model={survey} onComplete={onComplete} />
+      {chartData && (
+        <div className="chart-container">
+          <canvas id="survey-results-chart"></canvas>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/json.js
@@ -0,0 +1,187 @@
+export const surveyJson = {
+  pages: [{
+    elements: [{
+      type: "html",
+      html: "<h2>Teste de Personalidade.<br> Aponte as palavras que mais combinam com você!</h2>"
+    }]
+  }, {
+    elements: [{
+      name: "Esportiva",
+      title: "Selecione:",
+      type: "checkbox",
+      choices: [
+        "Casual",
+        "Alegre",
+        "Animada",
+        "Entusiasmada",
+        "Amigável",
+        "Divertida",
+        "Natural",
+        "Rápida",
+        "Otimista",
+        "Despretensiosa"
+      ],
+      "isRequired": false,
+      "colCount": 2
+      // "showNoneItem": true,
+      // "showOtherItem": true,
+      // "showSelectAllItem": true,
+      // "separateSpecialChoices": true
+    }]
+  }, {
+    elements: [{
+      name: "Tradiocinal",
+      title: "Selecione:",
+      type: "checkbox",
+      choices: [
+        "Sistemática",
+        "Conservadora",
+        "Confiável",
+        "Eficiente",
+        "Leal",
+        "Organizada",
+        "Fidedigna",
+        "Responsável",
+        "Sensível",
+        "Digna de Confiança"
+      ],
+      "isRequired": false,
+      "colCount": 2
+      // "showNoneItem": true,
+      // "showOtherItem": true,
+      // "showSelectAllItem": true,
+      // "separateSpecialChoices": true
+    }]
+  }, {
+    elements: [{
+      name: "Elegante",
+      title: "Selecione:",
+      type: "checkbox",
+      choices: [
+        "Educada",
+        "Digna",
+        "Perspicaz",
+        "Graciosa",
+        "Meticulosa",
+        "Preparada",
+        "Refinada",
+        "Reservada",
+        "Serena",
+        "Imponente"
+      ],
+      "isRequired": false,
+      "colCount": 2
+      // "showNoneItem": true,
+      // "showOtherItem": true,
+      // "showSelectAllItem": true,
+      // "separateSpecialChoices": true
+    }]
+  },
+  {
+    elements: [{
+      name: "Feminina/Romântica",
+      title: "Selecione:",
+      type: "checkbox",
+      choices: [
+        "Carinhosa",
+        "Compassiva",
+        "Atenciosa",
+        "Gentil",
+        "Romântica",
+        "Sensível",
+        "Suave",
+        "Apoiadora",
+        "Compreensiva",
+        "Terna"
+      ],
+      "isRequired": false,
+      "colCount": 2
+      // "showNoneItem": true,
+      // "showOtherItem": true,
+      // "showSelectAllItem": true,
+      // "separateSpecialChoices": true
+    }]
+  },
+  {
+    elements: [{
+      name: "Sensual/Magnética",
+      title: "Selecione:",
+      type: "checkbox",
+      choices: [
+        "Ousada",
+        "Atraente",
+        "Excitante",
+        "Galanteadora",
+        "Glamorosa",
+        "Provocante",
+        "Sedutora",
+        "Sensual",
+        "Sexy",
+        "Magnética"
+      ],
+      "isRequired": false,
+      "colCount": 2
+      // "showNoneItem": true,
+      // "showOtherItem": true,
+      // "showSelectAllItem": true,
+      // "separateSpecialChoices": true
+    }]
+  }, {
+    elements: [{
+      name: "Criativa",
+      title: "Selecione:",
+      type: "checkbox",
+      choices: [
+        "Aventureira",
+        "Artística",
+        "De espírito livre",
+        "Imaginativa",
+        "Independente",
+        "Inovadora",
+        "Original",
+        "Espontânea",
+        "Incomum",
+        "Ímpar"
+      ],
+      "isRequired": false,
+      "colCount": 2
+      // "showNoneItem": true,
+      // "showOtherItem": true,
+      // "showSelectAllItem": true,
+      // "separateSpecialChoices": true
+    }]
+  }, {
+    elements: [{
+      name: "Dramática",
+      title: "Selecione:",
+      type: "checkbox",
+      choices: [
+        "Agressiva",
+        "Desligada",
+        "Confidente",
+        "Exigente",
+        "Moderna",
+        "Intensa",
+        "Distante",
+        "Autoconfiante",
+        "Sofisticada",
+        "Cosmopolitana"
+      ],
+      "isRequired": false,
+      "colCount": 2
+      // "showNoneItem": true,
+      // "showOtherItem": true,
+      // "showSelectAllItem": true,
+      // "separateSpecialChoices": true
+    }]
+  }],
+  showQuestionNumbers: "on",
+  pageNextText: "Seguinte",
+  pagePrevText: "Anterior",
+  completeText: "Enviar",
+  showPrevButton: true,
+  firstPageIsStarted: true,
+  startSurveyText: "Iniciar",
+  completedHtml: "Obrigada!",
+  // showPreviewBeforeComplete: "showAnsweredQuestions"
+};
